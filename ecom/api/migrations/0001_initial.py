d175e922f17757b8e903dcc87a22ad10712bf4fb
Omarkhaledm21 <Omarkhaledm21@gmail.com>
2022-09-18 20:57:05 +0200

    full project

---
+++ ecom/api/migrations/0001_initial.py
@@ -0,0 +1,18 @@
+from django.db import migrations
+from api.user.models import CustomUser
+
+
+class Migration(migrations.Migration):
+    def seed_data(apps, schema_editor):
+        user = CustomUser(name='omar',
+                          email='test@test.dev',
+                          is_staff=True,
+                          is_superuser=True,
+                          phone='01010',
+                          gender='Male')
+        user.set_password('12345')
+        user.save()
+
+    dependencies = []
+
+    operations = [migrations.RunPython(seed_data), ]
